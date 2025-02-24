b3b4c8e92efbf9b839eee8b3d1fdc183f4c33f6c
charlie-lto <charlie@leedstestobjects.com>
2025-02-24 16:50:09 +0000

    feat: change to us small terminal over a floating terminal

--- init.lua
+++ init.lua
@@ -1,12 +1,16 @@
 require("config.lazy")
 require("cs-config")
+require('lualine').setup {}
 
 vim.cmd.colorscheme "catppuccin-mocha"
 
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
-require('lualine').setup {}
+-- sourcing files for changes
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
 
 
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -32,9 +36,12 @@ vim.api.nvim_create_autocmd('TermOpen', {
   end,
 })
 
-vim.keymap.set("n", "<leader>st", function()
+-- open terminal
+vim.keymap.set("n", "<M-i>", function()
   vim.cmd.vnew()
   vim.cmd.term()
   vim.cmd.wincmd("J")
   vim.api.nvim_win_set_height(0, 15)
 end)
+
+vim.keymap.set('t', '<M-i>', '<C-\\><C-n>:bd!<CR>,', { noremap = true, silent = true })
--- lua/plugins/floaterm.lua
+++
@@ -1,5 +0,0 @@
-return {
-  'voldikss/vim-floaterm',
-  vim.keymap.set("n", "<M-i>", vim.cmd.FloatermToggle),
-  vim.keymap.set("t", "<M-i>", vim.cmd.FloatermToggle),
-}
