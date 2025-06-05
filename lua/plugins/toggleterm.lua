98607bf2f2f75e1675de5c4b4a6d0d07690bd65f
charlie-lto <charlie@leedstestobjects.com>
2025-06-05 10:35:35 +0100

    fix: floating terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,10 +5,18 @@ return {
     opts = {
       --[[ things you want to change go here]]
       open_mapping = [[<M-i>]],
-      direction = 'vertical'
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      direction = "float",
+      float_opts = {
+        border = "curved",
+        winblend = 3,
+      },
+      shell = vim.fn.has("win32") == 1 and "powershell.exe" or vim.o.shell,
     },
-    config = function()
-      require("toggleterm").setup()
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
 
       vim.keymap.set("n", "<M-i>", vim.cmd.ToggleTerm)
       vim.keymap.set("t", "<M-i>", vim.cmd.ToggleTerm)
