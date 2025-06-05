cb944c32136ce0a75ad186f0d277573fe044960a
charlie-lto <charlie@leedstestobjects.com>
2025-06-05 10:33:45 +0100

    feat: smear cursor

---
+++ lua/plugins/smear.lua
@@ -0,0 +1,11 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  tag = "v0.4.6",
+  opts = {
+    --smear_between_buffers = true,
+    --smear_between_neighbor_lines = true,
+    --scroll_buffer_space = true,
+    --legacy_computing_symbols_support = false,
+    --smear_insert_mode = true,
+  },
+}
